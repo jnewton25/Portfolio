1ea7d94fa651b2cac4e82cb1e336fcfc73a393af
Justin Newton <justin-newton@outlook.com>
2022-08-01 13:37:34 -0500

    Grid Layout Started work.js

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { project } from "../../projects";
 import { Cards } from "../cards/cards";
-import Row from "react-bootstrap/Row";
+//import Row from "react-bootstrap/Row";
+import { Grid } from "@mui/material";
 
 export const Work = () => {
 	return (
-		<div className="container-fluid bg">
-			<Row>
+		<div className="container-fluid bg card-layout">
+			<Grid container>
 				{project.map((project) => (
 					<Cards key={project.id} project={project} />
 				))}
-			</Row>
+			</Grid>
 		</div>
 	);
 };
