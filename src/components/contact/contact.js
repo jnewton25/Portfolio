30881ab61fefd65326e2ceda9dae35a7e0e0e163
Justin Newton <justin-newton@outlook.com>
2022-08-08 14:21:19 -0500

    Updated Map & Form Initialization

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -27,38 +27,37 @@ export const Contact = () => {
 	};
 
 	return (
-		<div className="container-fluid bg">
+		<div className="container-fluid bg mt-5">
 			<div className="row about-home">
 				<div className="col">
 					<span className="section-span"></span>
 					<h2 className="section-header">Contact</h2>
-					<form ref={form} onSubmit={sendEmail}>
-						<label>Name</label>
-						<input
-							type="text"
-							name="user_name"
-							className="form-contact-input"
-						/>
-						<label>Email</label>
-						<input
-							type="email"
-							name="user_email"
-							className="form-contact-input"
-						/>
-						<label>Message</label>
-						<textarea
-							name="message"
-							className="form-contact-textarea"
-						/>
-						<input
-							type="submit"
-							value="Send"
-							className="form-contact-btn"
-						/>
-					</form>
+					<div className="form">
+						<form ref={form} onSubmit={sendEmail}>
+							<label>Name</label>
+							<input
+								type="text"
+								name="user_name"
+								className="input"
+							/>
+							<label>Email</label>
+							<input
+								type="email"
+								name="user_email"
+								className="input"
+							/>
+							<label>Message</label>
+							<textarea name="message" className="textarea" />
+							<input
+								type="submit"
+								value="Send"
+								className="submit-btn"
+							/>
+						</form>
+					</div>
 				</div>
 				<div className="form-map col">
-					<img src="/assets/map-iowa.jpg" alt="map" />
+					<img src="/assets/iowa-map.png" alt="map" />
 				</div>
 			</div>
 		</div>
